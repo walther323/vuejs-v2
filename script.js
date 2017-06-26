d9b92b67d5fb9b4c75fe9ef26e5430b4ff81c30a
walther <walter323@gmail.com>
2017-06-26 12:17:04 -0500

    Agrega elementos a una lista con VUEjs - V-IF, V-ELSE, @click, Métodos

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 new Vue({
 	el: '#appVue',
 	data:{
-		lists:[
-			{keep: 'uno', completed: true},
-			{keep: 'dos', completed: false},
-			{keep: 'tres', completed: true},
-		],
+		lists:[],
+		newKeep: ''
+	},
+	methods:{
+		addKeep: function()
+		{
+			this.lists.push({ 
+				keep: this.newKeep, completed: false
+			});
+			this.newKeep = '';
+		}
 	}
 });
